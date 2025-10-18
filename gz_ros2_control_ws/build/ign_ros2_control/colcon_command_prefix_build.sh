2e846f6c0a013b3896efc46c06e787505bc326c3
Ghostvulture <xluo799@connect.hkust-gz.edu.cn>
2025-10-18 17:58:31 +0800

    [add]bridge [TODO]ros2_control implement on fortress??

---
+++ gz_ros2_control_ws/build/ign_ros2_control/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/xyz/Desktop/xluo/Gazebo_test/gz_ros2_control_ws/install/gz_ros2_control/share/gz_ros2_control/package.sh"
---
+++ gz_ros2_control_ws/build/ign_ros2_control_demos/colcon_command_prefix_build.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/xyz/Desktop/xluo/Gazebo_test/gz_ros2_control_ws/install/gz_ros2_control/share/gz_ros2_control/package.sh"
+. "/home/xyz/Desktop/xluo/Gazebo_test/gz_ros2_control_ws/install/gz_ros2_control_demos/share/gz_ros2_control_demos/package.sh"
